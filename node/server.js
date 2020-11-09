0ab988eec1e62139467edba3f5afa7ff9d2367d5
Miha Bogataj <mb3750@student.uni-lj.si>
2020-11-09 12:49:47 +0100

    Fixed status sending

--- node/server.js
+++ node/server.js
@@ -12,64 +12,64 @@ app.use('/fonts', express.static(path.join("../fonts", '../fonts')));
 
 app.get('/', function (req, res) {
   fs.readFile('../docs/index.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/dashboard', function (req, res) {
   fs.readFile('../docs/dashboard.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/envelopes', function (req, res) {
   fs.readFile('../docs/envelopes.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/goals', function (req, res) {
   fs.readFile('../docs/goals.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/bills', function (req, res) {
   fs.readFile('../docs/bills.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/history', function (req, res) {
   fs.readFile('../docs/history.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/utility', function (req, res) {
   fs.readFile('../docs/utility.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.get('/account', function (req, res) {
   fs.readFile('../docs/account.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 200);
   });
 });
 
 app.all('*', function (req, res) {
   fs.readFile('../docs/404notfound.html', "utf8", function(err, data) {
-    sendFile(res, err, data);
+    sendFile(res, err, data, 404);
   });
 });
 
 app.listen(8080);
 
-function sendFile(res, err, data) {
+function sendFile(res, err, data, status) {
   if (err) {
     console.log(err);
-    res.sendStatus(404);
+    res.sendStatus(status);
     return;
   }
   res.send(data);
