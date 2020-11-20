15a9097f50ae5ba1f16ea5b0f8796b5171ca4702
Gašper <stepec.gasper97@gmail.com>
2020-11-20 23:57:34 +0100

    Validation check at SUBMIT

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,31 +3,72 @@ $(window).on('beforeunload', function() {
   $(window).scrollTop(0);
 });
 
-function nameRegex(username) {
+function disableButton(){
+  var button = document.getElementById("buttonup");
+  var name = nameRegex();
+  var surname = surnameRegex();
+  var email = emailCheckSignUp();
+  var password = passwordCheckSignUp();
+  var password2 = passwordStrength();
+
+  if (button == 0|| name == 0 || surname == 0 || email == 0 || password  == 0 || password2 == 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+function nameRegex() {
+  
+  var username = document.getElementById("nameup");
   var regex = new RegExp("^([a-zA-Z])+$");
   if(!username.value.match(regex)) {
-    username.borderColor = "red";
+    username.style.borderColor = "red";
     document.getElementById("alerts4").innerHTML ='<div class="alert alert-warning" role="alert">Name may only contain letters A-Z.</div>';
+    return 0;
   }
   else {
-    username.borderColor = "#ced4da";
+    username.style.borderColor = "#ced4da";
     document.getElementById("alerts4").innerHTML = "";
+    return 1;
   }
 }
-function surnameRegex(username) {
+function surnameRegex() {
+  var username = document.getElementById("surnameup");
   var regex = new RegExp("^([a-zA-Z])+$");
   if(!username.value.match(regex)) {
-    username.borderColor = "red";
+    username.style.borderColor = "red";
     document.getElementById("alerts5").innerHTML ='<div class="alert alert-warning" role="alert">Surname may only contain letters A-Z</div>';
+    return 0;
   }
   else {
-    username.borderColor = "#ced4da";
+    username.style.borderColor = "#ced4da";
     document.getElementById("alerts5").innerHTML = "";
+    return 1;
   }
 }
 
-function passwordStrength(password1) {
+function passwordCheckSignUp() {
+  var pass1 = document.getElementById("password1up");
+  var pass2 = document.getElementById("password2up");
+  if (pass1.value != pass2.value) {
+    pass1.style.borderColor = "red";
+    pass2.style.borderColor = "red";
+    var neki = document.getElementById("alerts3");
+    neki.innerHTML = ('<div class="alert alert-warning" role="alert">Passwords don\'t match!</div>');
+    return 0;
+  }
+  else {
+    pass1.style.borderColor = "#ced4da";
+    pass2.style.borderColor = "#ced4da";
+    document.getElementById("alerts3").innerHTML = "";
+    return 1;
+  }
+}
 
+function passwordStrength() {
+  //var button = document.getElementById("buttonup");
   //var email1 = document.getElementById(email1up);
   //var email2 = document.getElementById(email2up);
 
@@ -47,17 +88,22 @@ function passwordStrength(password1) {
   * - Include at least 1 lowercase letter AND 1 numeric character
   *  OR
   * - Include at least 1 uppercase letter AND 1 numeric character*/
-  if(password1.value.match(strongRegex)) {
-    password1.borderColor = "green";
+  var pass = document.getElementById("password1up");
+  if(pass.value.match(strongRegex)) {
+    pass.style.borderColor = "green";
     document.getElementById("alerts").innerHTML = "";
+    return 1;
   }
-  else if(password1.value.match(mediumRegex)) {
-    password1.borderColor = "orange";
+  else if(pass.value.match(mediumRegex)) {
+    
+    pass.style.borderColor = "orange";
     document.getElementById("alerts").innerHTML = "";
+    return 1;
   }
   else {
-    password1.borderColor = "red";
+    pass.style.borderColor = "red";
     document.getElementById("alerts").innerHTML = '<div class="alert alert-warning" role="alert">Strong passwords are at least 8 characters long, include at least 1 lowercase letter, 1 capital letter, 1 number and 1 special character => !@#$%^&**/</div>';
+    return 0;
   }
 }
 
@@ -68,26 +114,12 @@ function emailCheckSignUp() {
     email1.style.borderColor = "red";
     email2.style.borderColor = "red";
     document.getElementById("alerts2").innerHTML = '<div class="alert alert-warning" role="alert">E-mails don\'t match!</div>';
+    return 0;
   }
   else {
     email1.style.borderColor = "#ced4da";
     email2.style.borderColor = "#ced4da";
     document.getElementById("alerts2").innerHTML = "";
-  }
-}
-
-function passwordCheckSignUp() {
-  var pass1 = document.getElementById("password1up");
-  var pass2 = document.getElementById("password2up");
-  if (pass1.value != pass2.value) {
-    pass1.style.borderColor = "red";
-    pass2.style.borderColor = "red";
-    var neki = document.getElementById("alerts3");
-    neki.innerHTML = ('<div class="alert alert-warning" role="alert">Passwords don\'t match!</div>');
-  }
-  else {
-    pass1.style.borderColor = "#ced4da";
-    pass2.style.borderColor = "#ced4da";
-    document.getElementById("alerts3").innerHTML = "";
+    return 1;
   }
 }
