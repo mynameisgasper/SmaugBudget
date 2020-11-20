60d8626dd8ed885839ad1ac83a88d7388ff7fc2c
Miha Bogataj <mb3750@student.uni-lj.si>
2020-11-20 19:11:40 +0100

    Added dynamically generated data for history

--- app_server/controllers/history.js
+++ app_server/controllers/history.js
@@ -2,17 +2,87 @@
 var fs = require('fs');
 var responder = require('../routes/responder');
 
-function respond(res) {
-    res.render('history', {
-        title: 'history',
-        graph: {
-            used: true,
-            name: 'HistoryChart'
+var data = {
+    title: 'history',
+    graph: {
+        used: true,
+        name: 'HistoryChart'
+    },
+    dateRangePicker: {
+        used: true
+    },
+    expenses: [
+        {
+            id: 0,
+            year: '2020',
+            monthName: 'MAR',
+            month: '03',
+            day: '25',
+            category: 'Gas',
+            receiver: 'Petrol',
+            value: '50',
+            currency: '$'
         },
-        dateRangePicker: {
-            used: true
+        {
+            id: 1,
+            year: '2020',
+            monthName: 'MAR',
+            month: '03',
+            day: '25',
+            category: 'Phone',
+            receiver: 'A1',
+            value: '40',
+            currency: '$'
+        },
+        {
+            id: 2,
+            year: '2020',
+            monthName: 'MAR',
+            month: '03',
+            day: '26',
+            category: 'Groceries',
+            receiver: 'Mercator',
+            value: '150',
+            currency: '$'
+        },
+        {
+            id: 3,
+            year: '2020',
+            monthName: 'MAR',
+            month: '03',
+            day: '28',
+            category: 'Entertainment',
+            receiver: 'Kolosej',
+            value: '32',
+            currency: '$'
+        },
+        {
+            id: 4,
+            year: '2020',
+            monthName: 'MAR',
+            month: '03',
+            day: '28',
+            category: 'Eating out',
+            receiver: 'Pop\'s Place',
+            value: '63',
+            currency: '$'
+        },
+        {
+            id: 5,
+            year: '2020',
+            monthName: 'APR',
+            month: '04',
+            day: '1',
+            category: 'Groceries',
+            receiver: 'Mercator',
+            value: '120',
+            currency: '$'
         }
-    });
+    ]
+}
+
+function respond(res) {
+    res.render('history', data);
 }
 
 module.exports = {
