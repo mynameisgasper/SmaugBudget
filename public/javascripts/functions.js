a53e6e2c0d841fee02f55cef4af3193d4e598114
Miha Bogataj <mb3750@student.uni-lj.si>
2020-11-21 20:11:24 +0100

    Added x button script

---
+++ public/javascripts/functions.js
@@ -0,0 +1,5 @@
+function hideWelcome() {
+    document.getElementById('welcome-alert-section').style.display = 'none';
+    document.querySelectorAll('#cards-section')[0].style.marginTop = 10 + 'vh';
+    document.querySelectorAll('#friend-groups')[0].style.marginTop = 10 + 'vh';
+}
