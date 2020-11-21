75d34fc04886c6429962ed8929c2cc13f6e85138
Miha Bogataj <mb3750@student.uni-lj.si>
2020-11-21 20:13:53 +0100

    Removed unused row

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,5 +1,4 @@
 function hideWelcome() {
     document.getElementById('welcome-alert-section').style.display = 'none';
     document.querySelectorAll('#cards-section')[0].style.marginTop = 10 + 'vh';
-    document.querySelectorAll('#friend-groups')[0].style.marginTop = 10 + 'vh';
 }
