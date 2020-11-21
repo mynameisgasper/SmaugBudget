ecb7a69028450bfbebe24edc3020c5979d870132
Gašper <stepec.gasper97@gmail.com>
2020-11-21 20:32:34 +0100

    envelopes validation finished

--- public/javascripts/envelopes.js
+++ public/javascripts/envelopes.js
@@ -29,19 +29,19 @@ function amount(field) {
       return 1;
     }
 }
-function amount2(field) {
+function amount3(field,id) {
     //var field = document.getElementById("PayeeModal");
     var regex = new RegExp("^[0-9]+(\.[0-9]{1,2})?$"); 
     //decimalna števila z največj 2ma decimalnima mestoma ločilo je pika!
     //črkev male,velike,številke ne veljajo števila kot so .73, 
     if(!field.value.match(regex)) {
       field.style.borderColor = "red";
-      document.getElementById("amm2").innerHTML ='<div class="tekst" style="color:red">Input may only contain decimal numbers separated by \'.\' with max 2 decimal places.</div>';
+      document.getElementById("vnos"+id).innerHTML ='<div class="tekst" style="color:red">Input may only contain decimal numbers separated by \'.\' with max 2 decimal places.</div>';
       return 0;
     }
     else {
       field.style.borderColor = "#ced4da";
-      document.getElementById("amm2").innerHTML = "";
+      document.getElementById("vnos"+id).innerHTML = "";
       return 1;
     }
 }
