25aa7ef8c4b080d38cbd763b2e1151638d3efff9
Gašper <stepec.gasper97@gmail.com>
2020-11-22 19:53:54 +0100

    Welcome section delete remember

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,4 +1,15 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+$( window ).on( "load", function() {
+
+    if (sessionStorage.getItem(page) === "false") {
+        hideWelcome();
+    }
+})
+
 function hideWelcome() {
     document.getElementById('welcome-alert-section').style.display = 'none';
     document.querySelectorAll('#cards-section')[0].style.marginTop = 15 + 'vh';
+    sessionStorage.setItem(page, "false");
 }
