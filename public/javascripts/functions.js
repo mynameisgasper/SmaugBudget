c697d8cffdafc0765e0be7a163cc3b2f476776a6
Miha Bogataj <mb3750@student.uni-lj.si>
2020-11-22 11:26:12 +0100

    Fixed margins

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,4 +1,4 @@
 function hideWelcome() {
     document.getElementById('welcome-alert-section').style.display = 'none';
-    document.querySelectorAll('#cards-section')[0].style.marginTop = 10 + 'vh';
+    document.querySelectorAll('#cards-section')[0].style.marginTop = 15 + 'vh';
 }
