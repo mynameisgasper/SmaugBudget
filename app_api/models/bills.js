d666f2932f2f73c7941bef36f5cc8853e0219cdf
Tim Draksler <timson.draksler@gmail.com>
2020-11-23 18:57:27 +0100

    Added bills.js schema

---
+++ app_api/models/bills.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const billsSchema = new mongoose.Schema();
+billsSchema.add({
+    id: { type: Number, required: true },
+    recipient: { type: String, required: true },
+    value: { type: Number, required: true },
+    currency: { type: String, required: true },
+    date: { type: Date, required: true },
+    //  category: { type: categorySchema, required: true },
+});
+
+mongoose.model('bills', billsSchema, 'bills');
+
+module.exports = {
+    billsSchema: billsSchema
+}
