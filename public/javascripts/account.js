784a5dace4402d4fe302aca24349cfd8f0dbeb1c
lb8723 <lukabratina3@gmail.com>
2020-11-24 15:11:50 +0100

    pfp change 1.0

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -6,9 +6,24 @@ $(document).ready(function() {
             selector[i].innerText = selector[i].innerText.substring(2);
         }
     }
+
+    $("#addPfp").change(function(){
+        readURL(this);
+    });
 });
 
-function updateUsetInfo() {
+function updateUserInfo() {
     var forms = document.forms["userInfo"];
     console.log(forms);
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#profile-pic').attr('style', "background-image: url('" + e.target.result + "')").fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
 }
