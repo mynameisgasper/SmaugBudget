a53983e04b8a33f7bac6498390257c92b7770a2d
Gašper <stepec.gasper97@gmail.com>
2020-11-25 16:55:33 +0100

    API currency converter completed

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -7,21 +7,33 @@ $(window).on("load", function() {
     }
 
     if (window.location.pathname === '/utility') {
-        document.getElementById("convert").onclick = function() {
-            var currency1 = document.getElementById("curr1");
-            var currency2 = document.getElementById("curr2");
-            var amount = document.getElementById("Amount");
-            var amount2 = document.getElementById("Amount2");
-        
-            if (currency1.value === currency2.value) {
-            amount2.innerHTML = "1";
-            }
-            else {
-                $.get('/api/converter', {curr1:currency1, curr2:currency2, amm1:amount}, function (data, textStatus, jqXHR) {
-                    console.log(data);
-                });
-            }
+      document.getElementById("convert").onclick = function() {
+        var currency1 = document.getElementById("curr1");
+        var currency2 = document.getElementById("curr2");
+        var amount = document.getElementById("Amount");
+        var amount2 = document.getElementById("Amount2");
+    
+        if (currency1.value === currency2.value) {
+        amount2.innerHTML = "1";
         }
+        else {
+          $.ajax({
+            url: "/api/converter",
+            type: "get", //send it through get method
+            data: { 
+              curr1: currency1.value, 
+              curr2: currency2.value, 
+              amm1: amount.value
+            },
+            success: function(response) {
+              amount2.innerHTML = response.value;
+            },
+            error: function(xhr) {
+              //Do Something to handle error
+            }
+          });
+        }
+      }
     }
 })
 
