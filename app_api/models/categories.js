8fa40409c01047f93beaf07220347537fa538743
Grega Kranjec <grega.kranjec@outlook.com>
2020-11-27 22:36:43 +0100

    Changes to categories, basic categories implemented at registration

--- app_api/models/categories.js
+++ app_api/models/categories.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema();
 categorySchema.add({
     name: { type: String, required: true },
-    color: { type: String, required: true, default: 'rgb(47, 201, 30)' }
+    color: { type: String, required: true, default: 'rgb(47, 201, 30)' },
+    basic: { type: Boolean, required: true, default: false }
 });
 
 mongoose.model('Categories', categorySchema, 'Categories');
