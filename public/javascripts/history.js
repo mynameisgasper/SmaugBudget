ac6f5c58239215a45af2ac669883ae9abfaa6cd6
Gašper <stepec.gasper97@gmail.com>
2020-11-27 01:29:21 +0100

    table search

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -205,6 +205,32 @@ function dateCheck2(field) {
     }
 }
 
+function searchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
+
+        txtValue = td.textContent || td.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+}
+
+document.querySelector('#search').addEventListener('keyup', searchFunction, false);
+
+
 $(window).on("load", function() {
     if (sessionStorage.getItem(page) === "false") {
         hideWelcome();
