f1fbf35628a169d63048d251c28b55fd0b79861d
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-19 13:39:09 +0100

    Added basic router

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    FrameComponent,
+    HomeComponent, 
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [FrameComponent]
 })
 export class AppModule { }
