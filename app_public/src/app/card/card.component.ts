a6cc6adeba68ccd90152d9bccd51c2c51dc4e8ac
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-19 14:33:12 +0100

    Added card component

---
+++ app_public/src/app/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  @Input()
+  card: object;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
