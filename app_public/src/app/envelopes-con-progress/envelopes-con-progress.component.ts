0ddbb168974a6b7303291e1dfdb1b7b63d15f67f
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-21 21:35:35 +0100

    Implemented con progress

--- app_public/src/app/envelopes-con-progress/envelopes-con-progress.component.ts
+++ app_public/src/app/envelopes-con-progress/envelopes-con-progress.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faMinusSquare, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-envelopes-con-progress',
@@ -6,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./envelopes-con-progress.component.css']
 })
 export class EnvelopesConProgressComponent implements OnInit {
+
+  @Input()
+  data = {
+    "currency": "EUR",
+    "envelope": {}
+  }
+
+  faMinusSquare = faMinusSquare;
+  faPencilAlt = faPencilAlt
+
+  isLow(value: Number): Boolean {
+    return value < 85;
+  }
+
+  isMedium(value: Number): Boolean {
+    return value >= 85 && value < 100;
+  }
+
+  isHigh(value: Number): Boolean {
+    return value >= 100;
+  }
 
   constructor() { }
 
