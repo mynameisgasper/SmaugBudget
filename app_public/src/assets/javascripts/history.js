bbcaaa7ffe273d3d05e0638a9cdd2ca8033e5207
Gasper Stepec <stepec.gasper97@gmail.com>
2020-12-24 16:02:55 +0100

    Fixed history edit modals

--- app_public/src/assets/javascripts/history.js
+++ app_public/src/assets/javascripts/history.js
@@ -296,24 +296,24 @@ function disableButton2(id) {
     }
 }
 
-function nameAdd2(field) {
+function nameEditHistory(field) {
 
     //var field = document.getElementById("PayeeModal");
     var regex = new RegExp("^[A-Za-z0-9_@./#&+-: ]{1,20}$");
     //uppercase, lowercase, številke, posebni znaki, dolžina od 1-20
     if (!field.value.match(regex)) {
         field.style.setProperty("border-color", "red", "important");
-        $('.tt1').toast('show')
+        $(field.id).toast('show')
         return 0;
     } else {
         field.style.borderColor = "#ced4da";
-        $('.tt1').toast('hide')
+        $(field.id).toast('hide')
         return 1;
     }
 }
 
 
-function amount3(field) {
+function numberEditHistory(field) {
 
     //var field = document.getElementById("PayeeModal");
     var regex = new RegExp("^[0-9]+(\.[0-9]{1,2})?$");
@@ -321,16 +321,16 @@ function amount3(field) {
     //črkev male,velike,številke
     if (!field.value.match(regex)) {
         field.style.setProperty("border-color", "red", "important");
-        $('.tt2').toast('show')
+        $(field.id).toast('show')
         return 0;
     } else {
         field.style.borderColor = "#ced4da";
-        $('.tt2').toast('hide')
+        $(field.id).toast('hide')
         return 1;
     }
 }
 
-function dateCheck2(field) {
+function dateEditHistory(field) {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0');
@@ -338,33 +338,33 @@ function dateCheck2(field) {
     var inputDate = field.value.split("-");
 
     if (inputDate[0] < yyyy) {
-        $('.tt7').toast('hide');
+        $(field.id).toast('hide');
         field.style.borderColor = "#ced4da";
         return 1;
     } else if (inputDate[0] == yyyy) {
         if (inputDate[1] < mm) {
-            $('.tt7').toast('hide');
+            $(field.id).toast('hide');
             field.style.borderColor = "#ced4da";
             return 1;
         } else if (inputDate[1] == mm) {
             /* 
             ? IF DAY IS >= NOW */
             if (inputDate[2] <= dd) {
-                $('.tt7').toast('hide');
+                $(field.id).toast('hide');
                 field.style.borderColor = "#ced4da";
                 return 1;
             } else {
-                $('.tt7').toast('show');
+                $(field.id).toast('show');
                 field.style.setProperty("border-color", "red", "important");
                 return 0;
             }
         } else {
-            $('.tt7').toast('show');
+            $(field.id).toast('show');
             field.style.setProperty("border-color", "red", "important");
             return 0;
         }
     } else {
-        $('.tt7').toast('show');
+        $(field.id).toast('show');
         field.style.setProperty("border-color", "red", "important");
         return 0;
     }
