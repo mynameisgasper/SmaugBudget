999560166c368159ea4c2add448f8919535258ca
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-24 16:50:38 +0100

    Added REST API authentication

--- app_api/routers/apiRouter.js
+++ app_api/routers/apiRouter.js
@@ -7,12 +7,20 @@ var dbController = require('../controllers/dbController');
 var connections = require('../controllers/connectionsController');
 var categories = require('../controllers/categoryController');
 var friendGroup = require('../controllers/friendGroupController');
+const jwt = require('express-jwt');
 
 var express = require('express');
 const converter = require('../controllers/currencyConverter');
 
 var router = express.Router();
 router.use(express.json());
+
+const authentication = jwt({
+    secret: process.env.JWT_PASS || 'jwtsigntoken1!',
+    userProperty: 'payload',
+    algorithms: ['HS256']
+  });
+  
 
 //Index
 router.all('/', (req, res) => {
@@ -35,15 +43,15 @@ router.post('/resetPassword', (req, res) => {
     user.resetPassword(req, res);
 });
 
-router.post('/changePassword', (req, res) => {
+router.post('/changePassword', authentication, (req, res) => {
     user.changePassword(req, res);
 });
 
-router.post('/getUser/', (req, res) => {
+router.post('/getUser/', authentication, (req, res) => {
     user.retrieveUser(req, res);
 });
 
-router.post('/getUserEmail/', (req, res) => {
+router.post('/getUserEmail/', authentication, (req, res) => {
     user.retrieveUserEmail(req, res);
 });
 
@@ -51,98 +59,98 @@ router.all('/confirm/:urlCode/:code', (req, res) => {
     user.confirm(req, res);
 });
 
-router.post('/changeIncome', (req, res) => {
+router.post('/changeIncome', authentication, (req, res) => {
     user.changeIncome(req, res);
 });
 
-router.post('/uploadPfp', user.uploadImg, user.postImg);
+router.post('/uploadPfp', authentication, user.uploadImg, user.postImg);
 
-router.get('/getPfp', (req, res) => {
+router.get('/getPfp', authentication, (req, res) => {
     user.getPfp(req, res);
 });
 
-router.post('/updateUser', (req, res) => {
+router.post('/updateUser', authentication, (req, res) => {
     user.updateUser(req, res);
 });
 
-router.post('/setCurrency', (req, res) => {
+router.post('/setCurrency', authentication, (req, res) => {
     user.setCurrency(req, res);
 })
 
-router.post('/addBill', (req, res) => {
+router.post('/addBill', authentication, (req, res) => {
     bills.addBill(req, res);
 });
 
-router.post('/addEnvelope', (req, res) => {
+router.post('/addEnvelope', authentication, (req, res) => {
     envelopes.addEnvelope(req, res);
 });
 
-router.post('/editEnvelope', (req, res) => {
+router.post('/editEnvelope', authentication, (req, res) => {
     envelopes.editEnvelope(req, res);
 });
 
-router.post('/addExpense', (req, res) => {
+router.post('/addExpense', authentication, (req, res) => {
     envelopes.addExpense(req, res);
 });
 
-router.post('/getLastMonthExpenses', (req, res) => {
+router.post('/getLastMonthExpenses', authentication, (req, res) => {
     history.getLastMonthExpenses(req, res);
 });
 
-router.post('/deleteEnvelope', (req, res) => {
+router.post('/deleteEnvelope', authentication, (req, res) => {
     envelopes.deleteEnvelope(req, res);
 });
 
-router.post('/editExpense', (req, res) => {
+router.post('/editExpense', authentication, (req, res) => {
     history.editExpense(req, res);
 });
 
-router.get('/converter', (req, res) => {
+router.get('/converter', authentication, (req, res) => {
     converter.converter(req, res);
 });
 
-router.post('/editBill', (req, res) => {
+router.post('/editBill', authentication, (req, res) => {
     bills.editBill(req, res);
 });
 
-router.post('/deleteBill', (req, res) => {
+router.post('/deleteBill', authentication, (req, res) => {
     bills.deleteBill(req, res);
 });
 
-router.post('/addGoal', (req, res) => {
+router.post('/addGoal', authentication, (req, res) => {
     goals.addGoal(req, res);
 });
 
-router.post('/editGoal', (req, res) => {
+router.post('/editGoal', authentication, (req, res) => {
     goals.editGoal(req, res);
 });
 
-router.post('/addToGoalWithCategory', (req, res) => {
+router.post('/addToGoalWithCategory', authentication, (req, res) => {
     goals.addToGoalWithCategory(req, res);
 });
 
-router.post('/deleteGoal', (req, res) => {
+router.post('/deleteGoal', authentication, (req, res) => {
     goals.deleteGoal(req, res);
 });
 
 
-router.post('/removeAllDbData', (req, res) => {
+router.post('/removeAllDbData', authentication, (req, res) => {
     dbController.removeAllDbData(req, res);
 });
 
-router.get('/getNewUsers', (req, res) => {
+router.get('/getNewUsers', authentication, (req, res) => {
     connections.getNewUsers(req, res);
 });
 
-router.get('/getUserConnections', (req, res) => {
+router.get('/getUserConnections', authentication, (req, res) => {
     connections.getUserConnections(req, res);
 });
 
-router.post('/addConnection', (req, res) => {
+router.post('/addConnection', authentication, (req, res) => {
     connections.addConnection(req, res);
 });
 
-router.get('/getEnvelopesForDropdown', (req, res) => {
+router.get('/getEnvelopesForDropdown', authentication, (req, res) => {
     connections.getEnvelopesForDropdown(req, res);
 });
 
@@ -154,31 +162,31 @@ router.post('/loadCategories', (req, res) => {
     dbController.loadCategories(req, res);
 });
 
-router.post('/changeColorCategory', (req, res) => {
+router.post('/changeColorCategory', authentication, (req, res) => {
     categories.changeColorCategory(req, res);
 })
 
-router.post('/deleteCategory', (req, res) => {
+router.post('/deleteCategory', authentication, (req, res) => {
     categories.deleteCategory(req, res);
 });
 
-router.post('/toggleVisible', (req, res) => {
+router.post('/toggleVisible', authentication, (req, res) => {
     connections.toggleVisible(req, res);
 });
 
-router.post('/removeConnection', (req, res) => {
+router.post('/removeConnection', authentication, (req, res) => {
     connections.removeConnection(req, res);
 })
 
-router.post('/addFriendGroup', (req, res) => {
+router.post('/addFriendGroup', authentication, (req, res) => {
     friendGroup.addFriendGroup(req, res);
 })
 
-router.post('/calculateBalances', (req, res) => {
+router.post('/calculateBalances', authentication, (req, res) => {
     friendGroup.calculateBalances(req, res);
 })
 
-router.post('/deleteFriendGroup', (req, res) => {
+router.post('/deleteFriendGroup', authentication, (req, res) => {
     friendGroup.deleteFriendGroup(req, res);
 })
 
