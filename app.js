d20e73f6ef71e8c771faf000e6d1e2f072cd3513
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-29 17:53:44 +0100

    Added HTTPS redirect

--- app.js
+++ app.js
@@ -49,6 +49,16 @@ var swaggerOptions = {
 
 var app = express();
 
+// Preusmeritev na HTTPS na Heroku
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else
+      next();
+  });
+}
+
 //Currencies
 var currencies = [];
 
