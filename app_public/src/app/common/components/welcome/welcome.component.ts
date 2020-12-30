fc2c6a2ffabeb6ac3a40f9a5b9a3e77780cd0641
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-31 00:25:53 +0100

    Made an oopsie :)

--- app_public/src/app/common/components/welcome/welcome.component.ts
+++ app_public/src/app/common/components/welcome/welcome.component.ts
@@ -21,18 +21,18 @@ export class WelcomeComponent implements OnInit {
   ngOnInit(): void {
     this.show = localStorage.getItem(`${this.url}_welcome`);
     if (this.show == 'false') {
-      this.fixScreen();
+      this.fixScreen(0);
     }
   }
 
   hideWelcome() {
     this.show = 'false';
     localStorage.setItem(`${this.url}_welcome`, 'false');
-    this.fixScreen();
+    this.fixScreen(1);
   }
 
-  fixScreen() {
-    var element = this.document.querySelectorAll('.container')[1];
+  fixScreen(index: number) {
+    var element = this.document.querySelectorAll('.container')[index];
     element.setAttribute('style', 'margin-top:15vh!important');
     console.log(element);
   }
