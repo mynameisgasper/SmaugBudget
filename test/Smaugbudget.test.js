16860fc8c4cbeb4efeaa3ff37d015e91cb23f7e1
Miha Bogataj <mb3750@student.uni-lj.si>
2020-12-30 17:07:29 +0100

    Added another test case for login

--- test/Smaugbudget.test.js
+++ test/Smaugbudget.test.js
@@ -13,6 +13,17 @@ function takeScreenshot(browser, filename) {
   }));
 }
 
+function savePageSource(browser, filename) {
+  browser.getPageSource().then(result => fs.writeFile(filename, result, function(err) {
+    if (err) {
+      console.log("Error saving page source");
+    }
+    else {
+      console.log("Page source saved!");
+    }
+  }));
+}
+
 (async function SmaugBudget() {
     // Knjižnice
     fs = require('fs');
@@ -60,12 +71,78 @@ function takeScreenshot(browser, filename) {
         .usingServer(seleniumServerUrl)
         .build();
       });
-  
-      describe("Login", function() {
+
+      //Finished
+      /*describe("Register", function() {
         this.timeout(30 * 1000);
         before(() => { browser.get(applicationUrl); });
 
-        it("Open modal", async () => {
+        context("Register new user", function() {
+          it("Open modal", async () => {
+            await waitPageLoaded(browser, 10, "//h4");
+            await new Promise(r => setTimeout(r, 2000));
+            let registerButton = await browser.findElements(By.xpath("//a[contains(@href, '#registration')]"));
+            expect(registerButton).to.not.be.empty;
+            await registerButton[1].click();
+            let opened = await browser.findElements(By.xpath("//div[contains(@class, 'modal-backdrop fade show')]"));
+            expect(opened).to.not.be.empty;
+            await new Promise(r => setTimeout(r, 1000));
+          });
+        
+          it("Enter data", async () => {
+            let nameField = await browser.findElements(By.xpath("//input[contains(@id, 'nameup')]"));
+            expect(nameField).to.not.be.empty;
+            nameField[0].sendKeys("Bodza");
+            console.log(await browser.findElements(By.xpath("//input[contains(@id, 'nameup')]")));
+
+            let surnameField = await browser.findElements(By.xpath("//input[contains(@id, 'surnameup')]"));
+            expect(surnameField).to.not.be.empty;
+            surnameField[0].sendKeys("Reciever");
+
+            let email1Field = await browser.findElements(By.xpath("//input[contains(@id, 'email1up')]"));
+            expect(email1Field).to.not.be.empty;
+            email1Field[0].sendKeys("BodzaGolman@gmail.com");
+
+            let email2Field = await browser.findElements(By.xpath("//input[contains(@id, 'email2up')]"));
+            expect(email2Field).to.not.be.empty;
+            email2Field[0].sendKeys("BodzaGolman@gmail.com");
+
+            let password1Field = await browser.findElements(By.xpath("//input[contains(@id, 'password1up')]"));
+            expect(password1Field).to.not.be.empty;
+            password1Field[0].sendKeys("VelikaZoga1!");
+
+            let password2Field = await browser.findElements(By.xpath("//input[contains(@id, 'password2up')]"));
+            expect(password2Field).to.not.be.empty;
+            password2Field[0].sendKeys("VelikaZoga1!");
+
+            await new Promise(r => setTimeout(r, 1000));
+          });
+
+          it("Register", async () => {
+            let registerButton = await browser.findElements(By.xpath("//input[contains(@id, 'buttonid')]"));
+            expect(registerButton).to.not.be.empty;
+            await registerButton[0].click();
+          
+            await new Promise(r => setTimeout(r, 1000));
+  
+            takeScreenshot(browser, 'test/porocilo/data3.png')
+  
+            await new Promise(r => setTimeout(r, 3000));
+  
+            var url = await browser.getCurrentUrl();
+            console.log(url)
+            expect(url).to.include('confirm');
+          });
+        });
+      });*/
+  
+      //Finished
+      describe("Login", function() {
+        context("Fail login", function() {
+          this.timeout(30 * 1000);
+          before(() => { browser.get(applicationUrl); });
+  
+          it("Open modal", async () => {
             await waitPageLoaded(browser, 10, "//h4");
             let loginButton = await browser.findElements(By.xpath("//a[contains(text(), 'SIGN IN')]"));
             expect(loginButton).to.not.be.empty;
@@ -74,9 +151,58 @@ function takeScreenshot(browser, filename) {
             expect(opened).to.not.be.empty;
 
             await new Promise(r => setTimeout(r, 1000));
+          });
+
+          it("Enter data", async () => {
+            let emailField = await browser.findElements(By.xpath("//input[contains(@id, 'emailin')]"));
+            expect(emailField).to.not.be.empty;
+            emailField[0].sendKeys("nonexisting@smaug.com");
+
+            let passwordField = await browser.findElements(By.xpath("//input[contains(@id, 'passwordin')]"));
+            expect(passwordField).to.not.be.empty;
+            passwordField[0].sendKeys("Somethingrandom");
+            
+            await new Promise(r => setTimeout(r, 1000));
+          });
+
+          it("Login", async () => {
+            let loginButton = await browser.findElements(By.xpath("//input[contains(@value, 'Login')]"));
+            expect(loginButton).to.not.be.empty;
+            loginButton[0].click();
+            await new Promise(r => setTimeout(r, 1000));
+
+            var url = await browser.getCurrentUrl();
+            expect(url).to.not.include('dashboard');
+          });
+
+          it("Close modal", async() => {
+            let closeButton = await browser.findElements(By.xpath("//button[contains(@class, 'close')]"));
+            console.log(closeButton);
+            expect(closeButton).to.not.be.empty;
+            closeButton[0].click();
+
+            await new Promise(r => setTimeout(r, 1000));
+            let opened = await browser.findElements(By.xpath("//div[contains(@class, 'modal-backdrop fade show')]"));
+            expect(opened).to.be.empty;
+          });
         });
 
-        it("Enter data", async () => {
+        context("Successfully login Gold User", function() {
+          this.timeout(30 * 1000);
+          before(() => { browser.get(applicationUrl); });
+  
+          it("Open modal", async () => {
+            await waitPageLoaded(browser, 10, "//h4");
+            let loginButton = await browser.findElements(By.xpath("//a[contains(text(), 'SIGN IN')]"));
+            expect(loginButton).to.not.be.empty;
+            await loginButton[0].click();
+            let opened = await browser.findElements(By.xpath("//div[contains(@class, 'modal-backdrop fade show')]"));
+            expect(opened).to.not.be.empty;
+
+            await new Promise(r => setTimeout(r, 1000));
+          });
+
+          it("Enter data", async () => {
             let emailField = await browser.findElements(By.xpath("//input[contains(@id, 'emailin')]"));
             expect(emailField).to.not.be.empty;
             emailField[0].sendKeys("gold@smaug.com");
@@ -86,19 +212,21 @@ function takeScreenshot(browser, filename) {
             passwordField[0].sendKeys("Goldpass1");
             
             await new Promise(r => setTimeout(r, 1000));
-        });
+          });
 
-        it("Login", async () => {
+          it("Login", async () => {
             let loginButton = await browser.findElements(By.xpath("//input[contains(@value, 'Login')]"));
             expect(loginButton).to.not.be.empty;
             loginButton[0].click();
-            await new Promise(r => setTimeout(r, 10000));
-            //takeScreenshot(browser, 'test/login.png')
+            await new Promise(r => setTimeout(r, 1000));
+
             var url = await browser.getCurrentUrl();
             expect(url).to.include('dashboard');
+          });
         });
       });
       
+      //To be tested
       describe("Add new goal", function() {
         this.timeout(30 * 1000);
         before(() => { browser.get(applicationUrl); });
@@ -160,68 +288,6 @@ function takeScreenshot(browser, filename) {
 
       });
 
-      /*describe("Register", function() {
-        this.timeout(30 * 1000);
-        before(() => { browser.get(applicationUrl); });
-
-        it("Open modal", async () => {
-            await waitPageLoaded(browser, 10, "//h4");
-            let registerButton = await browser.findElements(By.xpath("//a[contains(@href, '#registration')]"));
-            expect(registerButton).to.not.be.empty;
-            await registerButton[1].click();
-            let opened = await browser.findElements(By.xpath("//div[contains(@class, 'modal-backdrop fade show')]"));
-            expect(opened).to.not.be.empty;
-
-            await new Promise(r => setTimeout(r, 1000));
-        });
-        
-        it("Enter data", async () => {
-          let nameField = await browser.findElements(By.xpath("//input[contains(@id, 'nameup')]"));
-          expect(nameField).to.not.be.empty;
-          nameField[0].sendKeys("Bodza");
-          console.log(await browser.findElements(By.xpath("//input[contains(@id, 'nameup')]")));
-
-          let surnameField = await browser.findElements(By.xpath("//input[contains(@id, 'surnameup')]"));
-          expect(surnameField).to.not.be.empty;
-          surnameField[0].sendKeys("Reciever");
-
-          let email1Field = await browser.findElements(By.xpath("//input[contains(@id, 'email1up')]"));
-          expect(email1Field).to.not.be.empty;
-          email1Field[0].sendKeys("BodzaGolman@gmail.com");
-
-          let email2Field = await browser.findElements(By.xpath("//input[contains(@id, 'email2up')]"));
-          expect(email2Field).to.not.be.empty;
-          email2Field[0].sendKeys("BodzaGolman@gmail.com");
-
-          let password1Field = await browser.findElements(By.xpath("//input[contains(@id, 'password1up')]"));
-          expect(password1Field).to.not.be.empty;
-          password1Field[0].sendKeys("VelikaZoga1!");
-
-          let password2Field = await browser.findElements(By.xpath("//input[contains(@id, 'password2up')]"));
-          expect(password2Field).to.not.be.empty;
-          password2Field[0].sendKeys("VelikaZoga1!");
-
-          await new Promise(r => setTimeout(r, 1000));
-        });
-
-        it("Confimr page", async () => {
-          let registerButton = await browser.findElements(By.xpath("//input[contains(@id, 'buttonid')]"));
-          expect(registerButton).to.not.be.empty;
-          await registerButton[0].click();
-        
-          await new Promise(r => setTimeout(r, 1000));
-
-          takeScreenshot(browser, 'test/data4.png')
-
-          await new Promise(r => setTimeout(r, 3000));
-
-          var url = await browser.getCurrentUrl();
-          console.log(url)
-          expect(url).to.include('confirm');
-      });
-      
-      });
-*/
       after(async () => {
         browser.quit();
       });
